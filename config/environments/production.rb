ffdb6c88322c8987207045a2acd25548e4a25d0d
tommybyrne <tbyrn687@gmail.com>
2015-01-14 22:14:37 -0600

    add production host for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://bloccitoff.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'tbyrne-listslayer.herokuapp.com'}
 end
